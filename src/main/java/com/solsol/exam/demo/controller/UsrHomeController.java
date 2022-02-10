046f4cb5725d4cf971b0a30149fd0689b406ca6f
solsolsol11 <ushio941@gmail.com>
2022-02-10 11:28:25 +0900

    다양한 리턴타입

--- src/main/java/com/solsol/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrHomeController.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 
 
@@ -69,18 +73,34 @@ public class UsrHomeController {
 	@RequestMapping("/usr/home/getArticle")
 	@ResponseBody
 	public Article getArticle() {
-		Article article = new Article();
+		Article article = new Article(1, "제목1");
+		
 
 		return article;
 	}
+	@RequestMapping("/usr/home/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "제목1");
+		Article article2 = new Article(2, "제목2");
+		
+		List<Article> list = new ArrayList<>();
+		list.add(article1);
+		list.add(article2);
+		
+		
+
+		return list;
+	}
 }
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 class Article{
+
 	private int id;
+
 	private String title;
-	
-	public Article() {
-		id = 1;
-		title = "제목1";
-	}
+
 }
