43135b4a9bb491b05563983abfe4b33c5f977977
solsolsol11 <ushio941@gmail.com>
2022-02-10 12:46:45 +0900

    게시물 상세보기 액션

--- src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
@@ -56,21 +56,22 @@ public class UsrArticleController {
 			}
 		}
 
-			return null;
+		return null;
 	}
-	
+
 	private void deleteArticle(int id) {
 		Article article = getArticle(id);
-		
-		articles.remove(article); 
-		
+
+		articles.remove(article);
+
 	}
+
 	private void modifyArticle(int id, String title, String body) {
 		Article article = getArticle(id);
-		
+
 		article.setTitle(title);
 		article.setBody(body);
-		
+
 	}
 	// 서비스 메서드 끝
 
@@ -89,38 +90,62 @@ public class UsrArticleController {
 		return articles;
 
 	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticleAction(int id) {
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+		
+		return article;
+		
+
+	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
 		Article article = getArticle(id);
-		
+
 		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
-		
+
 		deleteArticle(id);
-		
+
 		return id + "번 게시물을 삭제하였습니다.";
 	}
+
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
 		Article article = getArticle(id);
-		
+
 		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
-		
+
 		modifyArticle(id, title, body);
-		
+
 		return id + "번 게시물을 수정하였습니다.";
 	}
 
-	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
 
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
 
+		modifyArticle(id, title, body);
 
+		return id + "번 게시물을 수정하였습니다.";
+	}
 
 	// 액션 메서드 끝
 }
