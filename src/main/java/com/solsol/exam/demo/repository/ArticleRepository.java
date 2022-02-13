3bf990552326a4e646f0b3f1f066497aa5b4235e
solsolsol11 <ushio941@gmail.com>
2022-02-14 02:30:08 +0900

    SQL을 xml 파일로 이동

--- src/main/java/com/solsol/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/solsol/exam/demo/repository/ArticleRepository.java
@@ -13,26 +13,17 @@ import com.solsol.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
-	public void writeArticle(@Param("title")String title, @Param("body")String body);
 
-	
-	@Select("SELECT * FROM article WHERE id = #{id}")
-	public Article getArticle(@Param("id")int id);
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
 
-	@Delete("DELETE FROM article WHERE id = #{id}")
+	public Article getArticle(@Param("id") int id);
+
 	public void deleteArticle(int id);
 
-	@Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
-	public void modifyArticle(@Param("id")int id, @Param("title")String title, @Param("body")String body);
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
-	// 
-	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 
-
-	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
 
 }
