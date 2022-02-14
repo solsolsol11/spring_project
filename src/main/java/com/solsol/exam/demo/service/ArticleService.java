f6c2500a2fee034d0f6f94824a9215c6316c6418
solsolsol11 <ushio941@gmail.com>
2022-02-14 12:43:49 +0900

    usr/article/doAdd에 ResultData 적용

--- src/main/java/com/solsol/exam/demo/service/ArticleService.java
+++ src/main/java/com/solsol/exam/demo/service/ArticleService.java
@@ -3,11 +3,12 @@ package com.solsol.exam.demo.service;
 
 import java.util.List;
 
-
 import org.springframework.stereotype.Service;
 
 import com.solsol.exam.demo.repository.ArticleRepository;
+import com.solsol.exam.demo.util.Ut;
 import com.solsol.exam.demo.vo.Article;
+import com.solsol.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService { 
@@ -18,9 +19,11 @@ public class ArticleService {
 
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id = articleRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다", id), id);
 	}
 
 	public List<Article> getArticles() {
