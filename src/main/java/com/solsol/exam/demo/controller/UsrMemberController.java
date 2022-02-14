725b66b6702438f4e6242f87a34a66c9228aee1c
solsolsol11 <ushio941@gmail.com>
2022-02-14 10:39:22 +0900

    회원가입시, 중복 로그인아이디 체크

--- src/main/java/com/solsol/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrMemberController.java
@@ -7,28 +7,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.solsol.exam.demo.service.MemberService;
 import com.solsol.exam.demo.vo.Member;
 
-
-
-
-
 @Controller
 public class UsrMemberController {
 	private MemberService memberService;
-	
+
 	public UsrMemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+			String email) {
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		if (id == -1) {
+			return "해당 로그인아이디는 이미 사용중입니다.";
+		}
+
 		Member member = memberService.getMemberById(id);
-		
+
 		return member;
-		
+
 	}
-	
+
 }
-
-
