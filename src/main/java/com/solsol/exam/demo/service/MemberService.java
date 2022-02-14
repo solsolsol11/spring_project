ef6012498ed076afdb23c4fa13874bbb47279ea2
solsolsol11 <ushio941@gmail.com>
2022-02-14 10:13:47 +0900

    MemberRepository 추가 , 회원가입 처리

--- src/main/java/com/solsol/exam/demo/service/MemberService.java
+++ src/main/java/com/solsol/exam/demo/service/MemberService.java
@@ -2,14 +2,20 @@ package com.solsol.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.solsol.exam.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
-
-	// private MemberRepository memberRepository;
+	
+	private MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-			
-			//memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 
 }
