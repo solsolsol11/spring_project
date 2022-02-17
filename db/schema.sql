58984c7b58d4c9329014f35f2cdd5c792c200b62
solsolsol11 <ushio941@gmail.com>
2022-02-17 10:05:21 +0900

    게시물 테이블에 회원번호칼럼 추가

--- db/schema.sql
+++ db/schema.sql
@@ -85,4 +85,11 @@ nickname = '사용자2',
 `cellphoneNo` = '01011111111',
 email = 'ushio941@gmail.com';
 
-SELECT * FROM `member`
+# 게시물 테이블에  회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`
+SELECT * FROM article;
+
+# 기존 게시물의 작성자를 2번호로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
