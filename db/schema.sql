7192b4fe126a08256b7759e293aa5849f9ad8a5b
solsolsol11 <ushio941@gmail.com>
2022-02-17 13:40:05 +0900

    SQL 로거를 적용

--- db/schema.sql
+++ db/schema.sql
@@ -87,7 +87,7 @@ email = 'ushio941@gmail.com';
 
 # 게시물 테이블에  회원정보 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`
-SELECT * FROM article;
+
 
 # 기존 게시물의 작성자를 2번호로 지정
 UPDATE article
