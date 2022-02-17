4b8e67a76f2a46fb3be42dcc4a8dc50886b40d5d
solsolsol11 <ushio941@gmail.com>
2022-02-17 12:13:53 +0900

    게시물 삭제시 권한체크

--- src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
@@ -78,8 +78,25 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public ResultData<Integer> doDelete(int id) {
+	public ResultData<Integer> doDelete(HttpSession httpSesion,int id) {
+		
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if (httpSesion.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int)httpSesion.getAttribute("loginedMemberId");
+		}
+		
+		if(isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
+		
 		Article article = articleService.getArticle(id);
+		
+		if(article.getMemberId() != loginedMemberId) {
+			return ResultData.from("F-2", "권한이 없습니다.");
+		}
 
 		if (article == null) {
 			ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id));
