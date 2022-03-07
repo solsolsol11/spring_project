0340bd489f0030ca7776c38feb1d759887071cb9
solsolsol11 <ushio941@gmail.com>
2022-03-07 12:34:39 +0900

    NeedLogin인터셉터 구현 및 등록

--- src/main/java/com/solsol/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/solsol/exam/demo/config/MyWebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.solsol.exam.demo;
+package com.solsol.exam.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -6,15 +6,25 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.solsol.exam.demo.interceptor.BeforeActionInterceptor;
+import com.solsol.exam.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
+	// beforeActionInterceptor 인터셉터 불러오기
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
-	
+
+	// needLoginInterceptor 인터셉터 불러오기
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
-		.excludePathPatterns("/error");
+				.excludePathPatterns("/error");
+
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
 	}
 }
