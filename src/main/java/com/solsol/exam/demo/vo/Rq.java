48716b798dd33f1daf7a073b20d29f71d3c1491e
solsolsol11 <ushio941@gmail.com>
2022-03-07 11:17:44 +0900

    Rq 클래스 도입으로 로그인 여부와 관련된중복된 로직 제거

---
+++ src/main/java/com/solsol/exam/demo/vo/Rq.java
@@ -0,0 +1,30 @@
+package com.solsol.exam.demo.vo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+
+import lombok.Getter;
+
+public class Rq {
+	@Getter
+	private boolean isLogined;
+	@Getter
+	private int loginedMemberId;
+
+	public Rq(HttpServletRequest req) {
+		HttpSession httpSession = req.getSession();
+			boolean isLogined = false;
+			int loginedMemberId = 0;
+
+			if (httpSession.getAttribute("loginedMemberId") != null) {
+				isLogined = true;
+				loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+			}
+			
+			this.isLogined = isLogined;
+			this.loginedMemberId = loginedMemberId;
+	}
+
+}
