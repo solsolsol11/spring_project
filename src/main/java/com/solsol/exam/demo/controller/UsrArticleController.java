61e17f4be35c0e4f994770e689fb9cf310e7dd8c
solsolsol11 <ushio941@gmail.com>
2022-03-09 13:12:45 +0900

    게시물 작성시 게시판 선택

--- src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
@@ -153,7 +153,7 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public String doWrite( String title, String body, String replaceUri) {
+	public String doWrite(int boardId, String title, String body, String replaceUri) {
 		if (Ut.empty(title)) {
 			return rq.jsHistoryBack("title(을)를 입력해주세요.");
 		}
@@ -162,7 +162,7 @@ public class UsrArticleController {
 			
 		}
 
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), boardId, title, body);
 		int id = writeArticleRd.getData1();
 		
 		if (Ut.empty(replaceUri)) {
