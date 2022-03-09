30ada576e02c0435002f2d96f823cd4386b27981
solsolsol11 <ushio941@gmail.com>
2022-03-09 10:08:38 +0900

    데이지UI 적용

--- src/main/java/com/solsol/exam/demo/vo/Article.java
+++ src/main/java/com/solsol/exam/demo/vo/Article.java
@@ -17,5 +17,14 @@ public class Article {
 	private int memberId;
 	
 	private String extra__writerName;
+	private boolean extra__actorCanModify;
 	private boolean extra__actorCanDelete;
+	
+	public String getRegDateForPrint() {
+		return regDate.substring(2, 16);
+	}
+	
+	public String getUpdateDateForPrint() {
+		return updateDate.substring(2, 16);
+	}
 }
