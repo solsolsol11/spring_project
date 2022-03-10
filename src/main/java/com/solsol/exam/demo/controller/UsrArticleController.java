66fa34f892bc629096ead9b414da35191d1a1d69
solsolsol11 <ushio941@gmail.com>
2022-03-11 00:31:27 +0900

    조회수 증가 성공에 대한 답장에,data(글 번호) 정보 추가

--- src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
@@ -79,7 +79,11 @@ public class UsrArticleController {
 		}
 		
 		
-		return ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+		ResultData<Integer> rd = ResultData.newData(increaseHitCountRd, "hitCount", articleService.getArticleHitCount(id));
+		
+		rd.setData2("id", id);
+		
+		return rd;
 	}
 
 	@RequestMapping("/usr/article/getArticle")
