a3bf2572a9373d098ef32f26504a7b1eb03a6d47
solsolsol11 <ushio941@gmail.com>
2022-03-10 11:51:08 +0900

    리스트에서 검색타입과 검색어에 따라, 총 게시물 수 계산

--- src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/solsol/exam/demo/controller/UsrArticleController.java
@@ -32,7 +32,7 @@ public class UsrArticleController {
 	}
 
 	@RequestMapping("/usr/article/list")
-	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId,@RequestParam(defaultValue = "1") int page) {
+	public String showList(Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "title,Body")String searchKeywordTypeCode, @RequestParam(defaultValue = "")String searchKeyword ,@RequestParam(defaultValue = "1") int page) {
 		
 		Board board = boardService.getBoardById(boardId);
 		
@@ -41,7 +41,7 @@ public class UsrArticleController {
 		}
 		
 		
-		int articlesCount = articleService.getArticlesCount(boardId);
+		int articlesCount = articleService.getArticlesCount(boardId, searchKeywordTypeCode, searchKeyword);
 		
 		int itemsCountInAPage = 10;
 		int pagesCount = (int)Math.ceil((double)articlesCount / itemsCountInAPage);
