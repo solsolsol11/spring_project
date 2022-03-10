1b0cdebf768017730d90612d33f787bce76aae30
solsolsol11 <ushio941@gmail.com>
2022-03-10 12:57:08 +0900

    검색박스 구현

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -4,3 +4,14 @@ function check(form) {
 		return false;
 	}
 }
+
+$('select[data-value]').each(function(index, el){
+	const $el = $(el);
+	
+	const defaultValue = $el.attr('data-value').trim();
+	
+	if(defaultValue.length > 0){
+		$el.val(defaultValue);	
+	}
+	
+});
